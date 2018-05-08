8cde17df8da30b14b22c135addeca8a4ef2fcd12
fns92 <sanchez36822171@gmail.com>
2018-05-08 11:01:31 -0300

    fns92 : Class Binomio Test OK

--- Polinomios/src/polinomios/prueba.java
+++ Polinomios/src/polinomios/prueba.java
@@ -9,6 +9,12 @@ public class prueba {
 		Polinomios polinomio = new Polinomios(2,vector);
 		
 		System.out.println(polinomio.evaluarHorner(0));
+		
+		BinomioDeNewton bn1 = new BinomioDeNewton(1, 2, 3);
+		double res = bn1.obtenerCoeficiente(2);
+		System.out.println(res);
+		System.out.println(bn1.resolverBinomio(1));
+		bn1.toString();
 
 	}
 
